3f934d5307a2a78546284941d506a99be81a73ed
Ki Ki  Bush <donyabush@gmail.com>
2018-07-24 00:09:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your code in this file!
+function distanceFromHqInBlocks (pickUpLocation){
+  return Math.abs(pickUpLocation - 42);
+}
+
+function distanceFromHqInFeet (pickUpLocation){
+  return Math.abs(pickUpLocation - 42)*264;
+}
+
+function distanceTravelledInFeet (start, destination){
+  return Math.abs(start - destination)*264;
+}
+
+function calculatesFarePrice(start, destination){
+  if ((Math.abs(start - destination)*264) < 401){
+    return 0;
+  } 
+  else if ((Math.abs(start - destination)*264)>400 && (Math.abs(start - destination)*264)<2001){
+    return (0.02*(Math.abs(start - destination)*264 - 400));
+  }
+  else if ((Math.abs(start - destination)*264)>2000 && (Math.abs(start - destination)*264)<2501){
+  return 25;
+  }
+  else if ((Math.abs(start - destination)*264)>2000){
+    return 'cannot travel that far';
+  }
+}
